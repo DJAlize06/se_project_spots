efd30942f2176c0a7d00f4df7db12eac3a4db168
DJAlize <DJAlize06@yahoo.com>
2025-07-09 07:59:46 -0400

    after tutor session

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
-consteditProfileButton = document.querySelector(".profile__edit-button");
+const editProfileButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseButton = editProfileModal.querySelector(
   ".modal__close-button"
 );
 
-const newPostButton = document.querySelector(".profile__new-post-button");
+const newPostButton = document.querySelector(".profile__plus-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
 
-editProfileCloseButton.addEventListener("click", function () {
+editProfileButton.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -17,9 +17,9 @@ editProfileCloseButton.addEventListener("click", function () {
 });
 
 newPostButton.addEventListener("click", function () {
-  newPostButton.classList.add("modal_is-opened");
+  newPostModal.classList.add("modal_is-opened");
 });
 
 newPostCloseButton.addEventListener("click", function () {
-  newPostCloseButton.classList.remove("modal_is-opened");
+  newPostModal.classList.remove("modal_is-opened");
 });
